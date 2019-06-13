b195596d22b088eb4d8363e979e18800c4821d44
Rohan Pankaj <rohansp01@gmail.com>
2019-06-13 09:27:38 -0700

    end of ICS June 6

--- applicationMap.py
+++ applicationMap.py
@@ -1,8 +1,8 @@
 from user import user
-from task import task
-m_user = input("What is your name ") 
+from task import topic
+m_user = input("What is your name ")
 m_user = user()
 
 
-new_topic_def = task()
-#ef assign_topic_def():
+new_topic_def = topic()
+# ef assign_topic_def():
